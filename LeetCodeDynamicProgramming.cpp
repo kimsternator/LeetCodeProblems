920798ff4be5ca6a9131c13061e0fea2ceaa5405
kimsternator <sskim@ucsd.edu>
2020-10-17 11:24:51 -0700

    Summer 2020 p17

--- LeetCodeDynamicProgramming.cpp
+++ LeetCodeDynamicProgramming.cpp
@@ -108,4 +108,23 @@ bool canJump(vector<int>& nums) {
     return lastPos == 0;
 }
 
+//Unique Paths
+int uniquePaths(int m, int n) {
+    if(m == 0)
+        return 0;
+
+    vector< vector<int> > matrix(m, vector<int> (n, 0));
+
+    for(int i = 0; i < m; i++)
+        matrix[i][0] = 1;
+    for(int j = 0; j < n; j++)
+        matrix[0][j] = 1;
+
+    for(int i = 1; i < m; i++)
+        for(int j = 1; j < n; j++)
+            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+
+    return matrix[m - 1][n - 1];
+}
+
 
